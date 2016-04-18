08eca807512572785f9fe1f5aeb5ec5a6703f21a
Rolf Strijdhorst <rolfst@gmail.com>
2016-04-18 21:54:45 +0200

    added cache

--- C42_subscribedevents/events/urls.py
+++ C42_subscribedevents/events/urls.py
@@ -1,6 +1,7 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
+from django.views.decorators.cache import cache_page
 from . import views
 
 urlpatterns = [
-  url(r'^event-with-subscriptions/(?P<id>[a-z0-9_]+)', views.subscribed_events_detail, name='subscribed_event'),
+  url(r'^event-with-subscriptions/(?P<id>[a-z0-9_]+)', cache_page(252)(views.subscribed_events_detail), name='subscribed_event'),
 ]
